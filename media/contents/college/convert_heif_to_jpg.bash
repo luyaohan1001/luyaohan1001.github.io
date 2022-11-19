f7c397e6d58d303149759e137ef1ea839749cb01
Luyao Han <luyaohan1001@gmail.com>
2022-11-19 01:22:43 -0500

    Backup repo, delete when necessary.

--- media/contents/college/convert_heif_to_jpg.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *.heif; do
-	new_file_name=$(echo $file | sed 's/.heif//')
-	heif-convert -q 100 $file $new_file_name.jpg;
-done
--- media/contents/college/renumber_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in $(ls {*.jpg,*.JPG,*.png}); do 
-	newfileName=$(echo $file | sed 's/.jpg|.JPG|.png//')
-	mv $file $newfileName.png
-done
